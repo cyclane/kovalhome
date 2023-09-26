68aff49459f76c3d56c29b35281933124911058e
Gleb Koval <gleb@koval.net>
2023-09-26 19:50:11 +0000

    Cloud VM (#7)
    - Nextcloud & potentially other "cloud" stuff VM
    
    Reviewed-on: https://git.koval.net/cyclane/kovalhome/pulls/7

--- README.md
+++ README.md
@@ -7,4 +7,4 @@ This repository contains any automations used in deploying *.koval.net services.
 All 'managed' infrastructure is deployed and provisioned with [Ansible](https://www.ansible.com/).
 However, some 'unmanaged' (manually managed) resources also exist - primarily everything required for this repository to work (I don't want to make a dependency loop).
 
-Ansible playbooks are ran in alphanumerical order and are expected idempotent.
+Ansible playbooks are ran in alphanumerical order and are expected to be idempotent.
---
+++ infra/cloud/nextcloud/all-in-one.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+docker stop nextcloud-aio-mastercontainer || true
+docker rm nextcloud-aio-mastercontainer || true
+
+docker run \
+--init \
+--sig-proxy=false \
+--name nextcloud-aio-mastercontainer \
+--restart unless-stopped \
+--publish 8080:8080 \
+--env NEXTCLOUD_DATADIR=/mnt/data/nextcloud \
+--env NEXTCLOUD_UPLOAD_LIMIT=16G \
+--env NEXTCLOUD_MAX_TIME=7200 \
+--env APACHE_PORT=11000 \
+--env APACHE_IP_BINDING=0.0.0.0 \
+--env TZ=Europe/London \
+--env AIO_DISABLE_BACKUP_SECTION=true \
+--volume nextcloud_aio_mastercontainer:/mnt/docker-aio-config \
+--volume /var/run/docker.sock:/var/run/docker.sock:ro \
+-d nextcloud/all-in-one:latest
